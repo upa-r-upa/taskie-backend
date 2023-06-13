b253946f770c874cf8e54575467a1a4316771b1d
upa-r-upa <duekthtjf783@gmail.com>
2023-06-14 03:17:20 +0900

    [chore] Add flask ORM settings

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, request
-from flask_restx import Api, Resource
+from flask_restx import Api
 
-app = Flask(__name__)
+
+from . import create_app
+
+app = create_app()
 api = Api(app)
-
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=80)
