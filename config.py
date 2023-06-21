52eb6b2e5454d23324b54f8e0049e8b7cb987c3b
upa-r-upa <duekthtjf783@gmail.com>
2023-06-22 03:08:51 +0900

    feat: Add Login & Refresh token API

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+from datetime import timedelta
 
 BASE_DIR = os.path.dirname(__file__)
 
@@ -6,3 +7,9 @@ SQLALCHEMY_DATABASE_URI = "sqlite:///{}".format(
     os.path.join(BASE_DIR, "db", "taskie.db")
 )
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY", "비밀키")
+JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
+JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
+JWT_TOKEN_LOCATION = ["cookies"]
+JWT_ALGORITHM = "HS256"
