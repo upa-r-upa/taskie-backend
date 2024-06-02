f484b2f3d8b8c071228adfd032ef8e8c02cec122
upa-r-upa <me.uparupa@gmail.com>
2024-06-02 23:19:43 +0900

    feat: Add Create habit API

--- app/api/endpoints/__init__.py
+++ app/api/endpoints/__init__.py
@@ -5,6 +5,7 @@ from .health import router as health_router
 from .users import router as users_router
 from .todos import router as todos_router
 from .routines import router as routines_router
+from .habits import router as habits_router
 
 
 router = APIRouter()
@@ -15,3 +16,4 @@ router.include_router(health_router)
 router.include_router(users_router)
 router.include_router(todos_router)
 router.include_router(routines_router)
+router.include_router(habits_router)
--- app/repositories/__init__.py
+++ app/repositories/__init__.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import Session
 from app.core.auth import get_current_user
 from app.database.db import get_db
 from app.models.models import User
+from app.repositories.habit_repository import HabitRepository
 from .routine_repository import RoutineRepository
 
 
@@ -11,3 +12,9 @@ def get_routine_repository(
     db: Session = Depends(get_db), user: User = Depends(get_current_user)
 ):
     return RoutineRepository(db=db, user=user)
+
+
+def get_habit_repository(
+    db: Session = Depends(get_db), user: User = Depends(get_current_user)
+):
+    return HabitRepository(db=db, user=user)
