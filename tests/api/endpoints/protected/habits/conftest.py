ca7f4c0b47b647596bc918c74265b57d19bca4b0
upa-r-upa <me.uparupa@gmail.com>
2024-06-12 23:50:57 +0900

    test: Implement Get habit list API Tests

--- tests/api/endpoints/protected/habits/conftest.py
+++ tests/api/endpoints/protected/habits/conftest.py
@@ -1,9 +1,12 @@
-import pytest
+from datetime import datetime
+from typing import List
+from pytest import Session, fixture
 
+from app.models.models import Habit, HabitLog, User
 from app.schemas.habit import HabitCreateInput
 
 
-@pytest.fixture
+@fixture
 def habit_data() -> HabitCreateInput:
     return HabitCreateInput(
         title="물 마시기",
@@ -12,3 +15,126 @@ def habit_data() -> HabitCreateInput:
         repeat_time_minutes=30,
         repeat_days=[1, 2, 3, 4, 5, 6, 7],
     )
+
+
+@fixture
+def habit_list() -> List[Habit]:
+    return [
+        Habit(
+            title="습관 1",
+            start_time_minutes=480,
+            end_time_minutes=1380,
+            repeat_time_minutes=30,
+            repeat_days=[1, 2, 3, 4, 5, 6, 7],
+        ),
+        Habit(
+            title="습관 2",
+            start_time_minutes=600,
+            end_time_minutes=1380,
+            repeat_time_minutes=60,
+            repeat_days=[1, 2, 3, 4, 5],
+        ),
+        Habit(
+            title="습관 3",
+            start_time_minutes=960,
+            end_time_minutes=1380,
+            repeat_time_minutes=30,
+            repeat_days=[1, 2, 3, 4, 5, 6, 7],
+        ),
+        Habit(
+            title="습관 4",
+            start_time_minutes=960,
+            end_time_minutes=1380,
+            repeat_time_minutes=30,
+            repeat_days=[1, 2, 3, 4, 5, 6, 7],
+        ),
+        Habit(
+            title="습관 5",
+            start_time_minutes=960,
+            end_time_minutes=1380,
+            repeat_time_minutes=30,
+            repeat_days=[1, 2, 3, 4, 5, 6, 7],
+        ),
+    ]
+
+
+@fixture
+def habit_log_list() -> List[HabitLog]:
+    return [
+        HabitLog(
+            habit_id=1,
+            completed_at=datetime(2024, 6, 12, 485, 0, 0),
+        ),
+        HabitLog(
+            habit_id=1,
+            completed_at=datetime(2024, 6, 12, 520, 0, 0),
+        ),
+        HabitLog(
+            habit_id=1,
+            completed_at=datetime(2024, 6, 12, 540, 0, 0),
+        ),
+        HabitLog(
+            habit_id=1,
+            completed_at=datetime(2024, 6, 12, 575, 0, 0),
+        ),
+        HabitLog(
+            habit_id=1,
+            completed_at=datetime(2024, 6, 12, 720, 0, 0),
+        ),
+        HabitLog(
+            habit_id=2,
+            completed_at=datetime(2024, 6, 12, 485, 0, 0),
+        ),
+        HabitLog(
+            habit_id=2,
+            completed_at=datetime(2024, 6, 12, 520, 0, 0),
+        ),
+        HabitLog(
+            habit_id=2,
+            completed_at=datetime(2024, 6, 12, 540, 0, 0),
+        ),
+        HabitLog(
+            habit_id=2,
+            completed_at=datetime(2024, 6, 12, 575, 0, 0),
+        ),
+        HabitLog(
+            habit_id=2,
+            completed_at=datetime(2024, 6, 12, 720, 0, 0),
+        ),
+    ]
+
+
+@fixture
+def add_habit_log_list(
+    session: Session, habit_log_list: List[HabitLog]
+) -> List[HabitLog]:
+    session.add_all(habit_log_list)
+    session.commit()
+
+    return habit_log_list
+
+
+@fixture
+def add_habit_list(
+    habit_list: List[Habit],
+    session: Session,
+    add_user: User,
+) -> List[Habit]:
+    habit_list_items = []
+
+    for habit in habit_list:
+        habit_item = Habit(
+            title=habit.title,
+            start_time_minutes=habit.start_time_minutes,
+            repeat_time_minutes=habit.repeat_time_minutes,
+            repeat_days=habit.repeat_days,
+            user_id=add_user.id,
+        )
+
+        session.add(habit_item)
+
+        habit_list_items.append(habit_item)
+
+    session.commit()
+
+    return habit_list_items
--- tests/api/endpoints/protected/habits/test_habits.py
+++ tests/api/endpoints/protected/habits/test_habits.py
@@ -1,8 +1,15 @@
+from datetime import datetime
+from typing import List
 from fastapi.testclient import TestClient
 from pytest import Session
 
-from app.models.models import Habit
-from app.schemas.habit import HabitCreateInput, HabitDetail
+from app.models.models import Habit, HabitLog
+from app.schemas.habit import (
+    HabitCreateInput,
+    HabitDetail,
+    HabitListGetInput,
+    HabitWithLog,
+)
 
 
 def test_create_habit(
@@ -28,3 +35,38 @@ def test_create_habit(
     assert response_data.repeat_days == habit_data.repeat_days
 
     assert session.query(Habit).filter(Habit.id == response_data.id).first()
+
+
+def test_get_habits(
+    client: TestClient,
+    session: Session,
+    add_habit_list: list[Habit],
+    add_habit_log_list: list[HabitLog],
+    access_token_headers: dict[str, str],
+):
+    response = client.get(
+        "/habits/",
+        headers=access_token_headers,
+        params=HabitListGetInput(
+            limit=3,
+            log_target_date=datetime(2024, 6, 13),
+        ).dict(),
+    )
+
+    response_data: List[HabitWithLog] = [
+        HabitWithLog(**habit) for habit in response.json().get("data")
+    ]
+
+    assert response.status_code == 200
+
+    assert len(response_data) == 3
+    assert response_data[2].id == add_habit_list[0].id
+    assert response_data[1].id == add_habit_list[1].id
+    assert response_data[0].id == add_habit_list[2].id
+
+    assert len(response_data[0].log_list) == 5
+    assert len(response_data[1].log_list) == 5
+    assert len(response_data[2].log_list) == 0
+
+    assert response_data[0].log_list[0].id == add_habit_log_list[0].id
+    assert response_data[1].log_list[0].id == add_habit_log_list[5].id
